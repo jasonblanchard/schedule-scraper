af1339db1af16791fc3e04e13ced2ee9eecbb3c9
jwb82 <Jason@jason-blanchards-macbook-pro.local>
2012-10-30 11:16:19 -0400

    Put the app in a class, as it should

--- schedule_scraper_web.rb
+++ schedule_scraper_web.rb
@@ -6,89 +6,89 @@ require 'rack-flash'
 enable :sessions
 use Rack::Flash
 
+class ScheduleScraper
+    get '/' do
+        
+        @saved_searches = $saved_searches
 
-get '/' do
-    
-    @saved_searches = $saved_searches
-
-    erb :index
-end
-
-post '/' do
-    
-    $saved_searches.delete(params[:delete_saved_search])
-    flash.now[:notice] = "#{params[:delete_saved_search]} was deleted"
-
-    @saved_searches = $saved_searches
-    
-    erb :index
-end
-
-get '/output' do
-    redirect to('/')
-end
-
-post '/output' do
-    @courses = []
-    @total = 0
-
-    if params[:use_saved_values]
-        programs = $saved_searches[params[:name]]
-    else
-        programs = params["programs"]
+        erb :index
     end
 
-    if programs.respond_to?(:each)
+    post '/' do
+        
+        $saved_searches.delete(params[:delete_saved_search])
+        flash.now[:notice] = "#{params[:delete_saved_search]} was deleted"
 
-        programs.each do |k, program|
-            p = Program.new(program["program_uri"]) unless program["program_uri"].empty?
-            if p 
-                @courses << p.grab_courses(program["sections"],program["keyword"])
+        @saved_searches = $saved_searches
+        
+        erb :index
+    end
+
+    get '/output' do
+        redirect to('/')
+    end
+
+    post '/output' do
+        @courses = []
+        @total = 0
+
+        if params[:use_saved_values]
+            programs = $saved_searches[params[:name]]
+        else
+            programs = params["programs"]
+        end
+
+        if programs.respond_to?(:each)
+
+            programs.each do |k, program|
+                p = Program.new(program["program_uri"]) unless program["program_uri"].empty?
+                if p 
+                    @courses << p.grab_courses(program["sections"],program["keyword"])
+                end
             end
         end
-    end
-    
-    @courses.each do |courses|
-        courses.each do |program|
-            @total += 1
-        end
-    end
-
-    @timestamp = Time.new.to_i
-
-    Spreadsheet.create_sheet(@courses, @timestamp) unless @courses.empty?
-   
-    
-    if params["save"]
-        $saved_searches[params[:save_name]] = params[:programs]
-        file = File.new("./saved_searches.rb", "w")
-        file.syswrite("$saved_searches = #{$saved_searches}")
-    end
-
-    erb :output
-    
-end
-
-post '/download' do
-end
-
-get '/download' do
-    redirect to('/')
-end
-
-get '/admin' do
-    erb :admin
-end
-
-post '/admin' do
-
-    Dir.foreach('public/files') do |file|
-        next if file == '.' or file == '..' or file == '.gitignore'
-            File.delete("public/files/#{file}")
+        
+        @courses.each do |courses|
+            courses.each do |program|
+                @total += 1
+            end
         end
 
-        flash.now[:notice] = "All excel sheets destroyed"
+        @timestamp = Time.new.to_i
 
-    erb :admin
+        Spreadsheet.create_sheet(@courses, @timestamp) unless @courses.empty?
+       
+        
+        if params["save"]
+            $saved_searches[params[:save_name]] = params[:programs]
+            file = File.new("./saved_searches.rb", "w")
+            file.syswrite("$saved_searches = #{$saved_searches}")
+        end
+
+        erb :output
+        
+    end
+
+    post '/download' do
+    end
+
+    get '/download' do
+        redirect to('/')
+    end
+
+    get '/admin' do
+        erb :admin
+    end
+
+    post '/admin' do
+
+        Dir.foreach('public/files') do |file|
+            next if file == '.' or file == '..' or file == '.gitignore'
+                File.delete("public/files/#{file}")
+            end
+
+            flash.now[:notice] = "All excel sheets destroyed"
+
+        erb :admin
+    end
 end
-
